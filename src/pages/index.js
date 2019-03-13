c2585139e952bfbff8517736181ceae21dccd253
drkimpatrick <dr.kimanje2patrick2@gmail.com>
2019-03-13 19:10:06 +0300

    remove imports

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,8 @@
 import React from "react"
 import Layout from "../components/layout"
-import Icons from "../components/icons";
-import WelcomeInfo from "../components/welcomeInfo";
 
 const IndexPage = () => (
-  <Layout>
-  </Layout>
+  <Layout/>
 )
 
 export default IndexPage
-
