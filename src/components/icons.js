92ef6be21770b5fb88ea8e8245a06ffaba1d1b6d
drkimpatrick <dr.kimanje2patrick2@gmail.com>
2019-03-13 18:50:13 +0300

    make component self relient

--- src/components/icons.js
+++ src/components/icons.js
@@ -1,15 +1,41 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const IconTopWrapper = styled.div `
+  margin: 100px 0;
+
+  @media (max-width: 768px){
+    margin: 50px 0;
+  }
+`
+const IconInnerWrapper = styled.div `
+  display: grid;
+  grid-template-columns: repeat(5, auto);
+  justify-items: center;
+
+  @media (max-width: 768px){
+    grid-template-columns: repeat(3, 1fr);
+    grid-row-gap: 10px;
+    z-index: 1;
+  }
+`
+const IconLink = styled.a ``
+const IconImage = styled.img `
+  background-color: none;
+  border-radius: 50%;
+  box-shadow: 2px 2px 10px black;
+`
 
 const Icons = () => (
-    <div className='iconTopWrapper'>
-        <div className='iconInnerWrapper'>
-          <a href='https://github.com/DrKimpatrick'><img src={require('../images/github2.png')} alt='social' width='50px' /></a>
-          <a href='https://web.facebook.com/dr.kimpatrick'><img src={require('../images/facebook2.png')} alt='social' width='50px' /></a>
-          <a href='https://twitter.com/DrKimpatrick'><img src={require('../images/twitter2.png')} alt='social' width='50px' /></a>
-          <a href='https://www.linkedin.com/in/drkimpatrick'><img src={require('../images/linkedin.png')} alt='social' width='50px' /></a>
-          <a href='https://profile.codersrank.io/user/drkimpatrick/asOthersSee'><img src={require('../images/coderank.jpg')} alt='social' width='50px' /></a>
-        </div>
-      </div>
+    <IconTopWrapper>
+        <IconInnerWrapper>
+            <IconLink href='https://github.com/DrKimpatrick'><IconImage src={require('../images/github2.png')} alt='social' width='50px' /></IconLink>
+            <IconLink href='https://web.facebook.com/dr.kimpatrick'><IconImage src={require('../images/facebook2.png')} alt='social' width='50px' /></IconLink>
+            <IconLink href='https://twitter.com/DrKimpatrick'><IconImage src={require('../images/twitter2.png')} alt='social' width='50px' /></IconLink>
+            <IconLink href='https://www.linkedin.com/in/drkimpatrick'><IconImage src={require('../images/linkedin.png')} alt='social' width='50px' /></IconLink>
+            <IconLink href='https://profile.codersrank.io/user/drkimpatrick/asOthersSee'><IconImage src={require('../images/coderank.jpg')} alt='social' width='50px' /></IconLink>
+        </IconInnerWrapper>
+    </IconTopWrapper>
 )
 
 export default Icons;
--- src/components/welcomeInfo.js
+++ src/components/welcomeInfo.js
@@ -1,13 +1,84 @@
 import React from 'react';
-import { Link } from "gatsby";
 import welcomeData from './staticProjectData.json';
+import styled, { keyframes } from 'styled-components';
+
+const welcomeAnimation = keyframes `
+    0% {
+        opacity: 0;
+        transform: translateY(20px);
+    }
+    100% {
+        opacity: 1;
+        transform: translateY(0px);
+    }
+`
+const WrapperTop = styled.div ``
+const Title = styled.h2 `
+    margin: 0 0 5%;
+    font-size: 50px;
+    font-weight: 700;
+    line-height: 1.2;
+    background: -webkit-linear-gradient(rgba(48, 69, 92, 0.9), #6dd5ed);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+    opacity: 0;
+    animation: ${welcomeAnimation} 0.8s ease-in 0s forwards;
+
+    @media (max-width: 768px){
+        font-size: 30px;
+    }
+`
+const Description = styled.p `
+    margin: 0 0 20%;
+    font-size: 26px;
+    font-weight: 700;
+    line-height: 1.2;
+    background: -webkit-linear-gradient(rgba(48, 69, 92, 0.9), #525c65);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+    opacity: 0;
+    animation: ${welcomeAnimation} 0.8s ease-in 0s forwards;
+
+    @media (max-width: 768px){
+        font-size: 20px;
+    }
+`
+const Button = styled.a `
+    font-size: 17px;
+    font-weight: 600;
+    color: white;
+    background: #525c65;
+    padding: 9px 20px;
+    text-transform: uppercase;
+    border: 1px solid rgb(255, 255, 255, 0.25);
+    border-radius: 20px;
+    text-decoration: none;
+    opacity: 0;
+    animation: ${welcomeAnimation} 0.8s ease-in-out 0s forwards;
+
+    :hover {
+        background-color: white;
+        color: #525c65;
+        box-shadow: 0 0 5px black;
+        transition: 1s cubic-bezier(0.175, 0.885, 0.32, 1.275);
+        }
+    
+    @media (max-width: 768px){
+        font-size: 14px;
+    }
+
+    @media (max-width: 400px){
+        font-size: 10px;
+    }
+
+`
 
 const WelcomeInfo = () => (
-    <>
-        <h1>{welcomeData.introductionData.title}</h1>
-        <p>{welcomeData.introductionData.description}</p>
-        <Link to="/page-2/" id='videLink'>please get in touch</Link>
-    </>
+    <WrapperTop>
+        <Title>{welcomeData.introductionData.title}</Title>
+        <Description>{welcomeData.introductionData.description}</Description>
+        <Button href="#contact" id='contactButton'>please get in touch</Button>
+    </WrapperTop>
 )
 
 export default WelcomeInfo;
